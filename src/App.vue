2ffe07ee64be7b2ddc645cf30f6cfce872c612d5
Dina El Badri <dinaelbadri94@gmail.com>
2022-12-14 09:35:10 +0100

    heey

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,13 @@
 
   <RouterView />
 
-  <NavbarPhone />
+
   
 </template>
 
 <script>
-import NavbarPhone from "./components/NavbarPhone.vue"
 export default {
-  components:{
-    NavbarPhone,
-  },
+
 data(){
   return{
 
