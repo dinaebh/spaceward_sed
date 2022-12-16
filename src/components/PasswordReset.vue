92a67451a93f6f9030c3f0573f98cd5d32a62dab
Estel CM <estel_927@hotmail.com>
2022-12-16 15:30:15 +0100

    password start

---
+++ src/components/PasswordReset.vue
@@ -0,0 +1,30 @@
+<template>
+
+</template>
+<script>
+
+import { mapStores } from "pinia";
+  import userStore from "../stores/user.js";
+
+  export default {
+    data() {
+      return {
+        password: "",
+        email: "",
+      };
+      
+    },
+    computed: {
+      ...mapStores(userStore),
+    },
+
+    methods: {
+      logInCall() {
+        this.userStore.logIn(this.email, this.password);
+      },
+    },
+}
+</script>
+
+<style>
+</style>
