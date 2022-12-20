56c148f412747b4c27c86d340660231771b4ce21
Sara El Badri <saraelbadri.ci@gmail.com>
2022-12-20 17:47:02 +0100

    sidebar almost finished

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,104 +1,125 @@
 <template>
-<!-- <div class="sidebar" :style="{width: sidebarWidth}">
-    <span v-if="collapsed">
-        <div>LOGO</div>
-    </span>
-    <span v-else>
-        <div><h1>SpaceWard</h1></div>
-    </span>
-    <SidebarLink to="/" icon="">Dashboard</SidebarLink>
-    <SidebarLink to="/userscreen" icon="fa-solid fa-user">Dashboard</SidebarLink>
-    <SidebarLink to="/" icon="">Dashboard</SidebarLink>
-    
-
-
-
-
-
-    <span
-    class="collapse-icon"
-    :class="{'rotate-180' : collapsed}"
-    @click="collapsed = !collapsed">
-    <i class="fa-sharp fa-solid fa-arrow-right"></i>
-
-    </span>
-</div> -->
-
+  <div class="navigation" v-bind:class="{ active: isActive }">
+    <div class="menuToggle" @click="isActive = !isActive"></div>
+    <SidebarLink/>
+</div>
+  
 </template>
 <script>
-
-
-export default{
-    components:{
-        
+import SidebarLink from "./SidebarLink.vue"
+export default {
+  components: {
+    SidebarLink,
+  },
+  data() {
+    return {
+     
+      isActive: false,
+    };
+  },
+  computed: {
+   
+  },
+  methods: {
+   
+    IsActive() {
+      this.isActive = !this.isActive;
     },
-    data(){
-        // return{
-        //     collapsed: false,
-        //     SIDEBAR_WIDTH: 180,
-        //     SIDEBAR_WIDTH_COLLAPSED: 58,
-          
-        // }
-    },
-    computed: {
-        // sidebarWidth(){
-        //     return `${this.collapsed? this.SIDEBAR_WIDTH_COLLAPSED : this.SIDEBAR_WIDTH}px`
-        // },
-
-
-    },
-    methods:{
-        // toggleSidebar(){
-        //     this.collapsed= !collapsed;
-        // }
-    }
     
-}
+    
+  },
+};
 </script>
 <style>
-/* :root{
-    --sidebar-bg-color: #D9D9D9;
-    --sidebar-item-hover: #dacff5;
-    --sidebar-item-active: #4a2b6f;
-
+:root {
+  --bg: #190c3d;
 }
 </style>
 <style scoped>
-
-
-.sidebar{
-    color: #11042b;
-    background-color: var(--sidebar-bg-color);
-
-    float: left;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    padding: 0.5em;
-
-    transition: 0.3s ease;
-
-    display: flex;
-    flex-direction: column;
-    color: rgb;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins";
+}
+body {
+  min-height: 100vh;
+  background: var(--bg);
 }
 
-.collapse-icon{
-    position: absolute;
-    bottom: 0;
-    padding: 0.75em;
-
-    color: rgb(23, 78, 120, 0.7);
-
-    transition: 0.2s linear;
+.navigation {
+  position: fixed;
+  inset: 40px 0 40px 20px;
+  background: #fff;
+  width: 75px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.navigation.active {
+  width: 250px;
+}
+.menuToggle {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  padding: 0 23px;
+  border-bottom: 1px solid rgb(0, 0, 0, 0.25);
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  cursor: pointer;
 }
 
-.rotate-180{
-    transform: rotate(-180deg);
-    transition: 0.2s linear;
-} */
+.menuToggle::before {
+  content: "";
+  position: absolute;
+  width: 30px;
+  height: 2px;
+  background:#333;
+  transform: translateY(-8px);
+  transition: 0.5s;
+}
+
+.navigation.active .menuToggle::before {
+  transform: translateY(0) rotate(45deg);
+}
+.menuToggle::after {
+  content: "";
+  position: absolute;
+  width: 30px;
+  height: 2px;
+  background: #333;
+  transform: translateY(8px);
+  box-shadow: 0 -8px 0 #333;
+  transition: 0.5s;
+}
+
+.navigation.active .menuToggle::after {
+  transform: translateY(0) rotate(-45deg);
+  box-shadow: 0 0 0 #333;
+}
+
+.navigation ul{
+display: flex;
+flex-direction: column;
+width: 100%;
+}
+.navigation ul li{
+    list-style: none;
+    position: relative;
+    width: 100%;
+    height: 76px;
+    border-radius: 12px;
+    border: 8px solid transparent;
+    transition: 0.5s;
 
 
+}
+.navigation ul li.active{
+    transform: translateX(30px);
+    background: var(--bg);
+}
 </style>
