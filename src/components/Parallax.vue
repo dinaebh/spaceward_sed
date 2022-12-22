eaf30dc4d28f0ede620f4e8820368760c598f1ee
Sara El Badri <saraelbadri.ci@gmail.com>
2022-12-22 17:02:41 +0100

    added cards

--- src/components/Parallax.vue
+++ src/components/Parallax.vue
@@ -1,12 +1,30 @@
 <template>
-<div class="wrapper">
-    
-    <div class="typing-demo">
-      Come on! We're almost done!
+
+<div class="all-cards">
+
+   <div class="card1 card">
+    <img src="../assets/img/card1.png" alt="comunication">
+    <div class="container">
+        <h4>Comunicate</h4>
+        <p>Connect with your friends and organize your projects together</p>
     </div>
-   <div class="animation">
-    This is the end!
-</div>
+   </div>
+
+   <div class="card2 card">
+    <img src="../assets/img/card2.png" alt="comunication">
+    <div class="container">
+        <h4>Comunicate</h4>
+        <p>Connect with your friends and organize your projects together</p>
+    </div>
+   </div>
+
+   <div class="card3 card">
+    <img src="../assets/img/card3.png" alt="comunication">
+    <div class="container">
+        <h4>Comunicate</h4>
+        <p>Connect with your friends and organize your projects together</p>
+    </div>
+   </div>
 </div>
 </template>
 <script>
@@ -20,55 +38,79 @@ export default {
 </script>
 
 <style scoped>
-.wrapper {
-  height: 100vh;
-  /*This part is important for centering*/
-  display: grid;
-  place-items: center;
-
+.card1 {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 0px 20px 20px 0px;
+  width: 80vw;
+  background-color: #fff0cc;
+  margin: 20px;
+  display: flex;
+  flex-direction: row;
+  border-left: 10px solid #ffb400;
+  padding: 20px;
 }
-.typing-demo {
-  width: 27ch;
-  animation: typing 2s steps(27), blink .5s step-end infinite alternate;
-  white-space: nowrap;
-  overflow: hidden;
-  border-right: 3px solid;
-  font-family: monospace;
-  font-size: 2em;
-  animation-delay: 1s;
-
+.card2 {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 0px 20px 20px 0px;
+  width: 80vw;
+  background-color: #cceff9;
+  margin: 20px;
+  display: flex;
+  flex-direction: row;
+  border-left: 10px solid #00aee0;
+  padding: 20px;
 }
-
-.animation {
-  width: 27ch;
-  animation: fadeIn 0.25s, typing 2s steps(27), blink .5s step-end infinite alternate;
-  white-space: nowrap;
-  overflow: hidden;
-  border-right: 3px solid;
-  font-family: monospace;
-  font-size: 2em;
-  animation-delay: 3s;
-  opacity: 0;
-  animation-fill-mode: forwards;
- 
- 
-}
-
-@keyframes typing {
-  from {
-    width: 0
-  }
-}
-    
-@keyframes blink {
-  50% {
-    border-color: transparent
-  }
+.card3 {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 0px 20px 20px 0px;
+  width: 80vw;
+  background-color: #ffd8cc;
+  margin: 20px;
+  display: flex;
+  flex-direction: row;
+  border-left: 10px solid #ff3a00;
+  padding: 20px;
 }
 
-  @keyframes fadeIn {
-  0% { opacity: 0;}
-  100% {opacity: 1;}
-
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
 }
+
+.container {
+  padding: 2px 16px;
+}
+
+.card img {
+  width: 30%;
+  object-fit: cover;
+}
+
+.embed-responsive .card > img {
+  object-fit: cover;
+}
+
+h4 {
+  font-size: 30px;
+  font-weight: 700;
+  line-height: 90px;
+  color: #292e47;
+}
+.container p {
+  color: #6b708d;
+  font-size: 15px;
+  font-weight: 400;
+  line-height: 20px;
+}
+
+.container a {
+  text-decoration: none;
+  color: #072ac8;
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 60px;
+}
+
 </style>
